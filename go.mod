9edc703c327f0b0d9b4116ae90089d458f866fe9
jordenskraften <97627971+jordenskraften@users.noreply.github.com>
2024-04-02 14:42:58 +0600

    much better repo designed for any kind of database

--- go.mod
+++ go.mod
@@ -2,10 +2,12 @@ module pxgpool-crud-tests
 
 go 1.21.4
 
-require github.com/jackc/pgx/v5 v5.5.5
+require (
+	github.com/Masterminds/squirrel v1.5.4
+	github.com/jackc/pgx/v5 v5.5.5
+)
 
 require (
-	github.com/Masterminds/squirrel v1.5.4 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
--- internal/repository/example/example_repo_postgres.go
+++ internal/repository/example/example_repo_postgres.go
@@ -56,28 +56,3 @@ func (ex *ExampleRepositoryPostgres) GetRandomQuestion() (*model.Question, error
 
 	return &question, nil
 }
-
-/*
-func (repo *Repository) GetRandomQuestion() (*model.Question, error) {
-	// Запрос для получения случайной записи
-	query := "SELECT id, question_title, question_text, answer_text FROM Question ORDER BY RANDOM() LIMIT 1"
-
-	// Подготовка структуры для хранения результата
-	var question model.Question
-
-	// Выполнение запроса
-	err := repo.db.Pool.QueryRow(context.Background(), query).Scan(
-		&question.Id,
-		&question.Question_title,
-		&question.Question_text,
-		&question.Answer_text,
-	)
-	repo.logger.Info("Fetched question: %+v\n", question)
-	if err != nil {
-		repo.logger.Error("Failed to fetch random question: ", err)
-		return nil, err
-	}
-
-	return &question, nil
-}
-*/
